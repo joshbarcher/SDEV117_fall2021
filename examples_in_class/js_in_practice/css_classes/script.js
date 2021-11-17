49b4ada863ffec915d6667641c80d263d873a326
Josh Archer <jarcher@greenriver.edu>
2021-11-17 10:53:19 -0800

    Added an example of setting css classes in JS.

---
+++ examples_in_class/js_in_practice/css_classes/script.js
@@ -0,0 +1,46 @@
+let lists = document.querySelectorAll("ul");
+
+for (let i = 0; i < lists.length; i++)
+{
+    if (i % 2 === 0) //i is even
+    {
+        lists[i].className = "dark";
+    }
+    else //i is odd
+    {
+        lists[i].className = "light";
+    }
+}
+
+/*
+let listClassName = "";
+if (confirm("Do you like darker colors?"))
+{
+    listClassName = "dark";
+}
+else
+{
+    listClassName = "light";
+}
+
+for (let i = 0; i < lists.length; i++)
+{
+    lists[i].className = listClassName;
+}*/
+
+/*if (confirm("Do you like darker colors?"))
+{
+    list.style.backgroundColor = "lightgray";
+    list.style.border = "1px solid black";
+    list.style.padding = "10px";
+    list.style.color = "darkblue";
+    list.style.listStyleType = "none";
+}
+else
+{
+    list.style.backgroundColor = "lightyellow";
+    list.style.border = "1px solid black";
+    list.style.padding = "10px";
+    list.style.color = "orange";
+    list.style.listStyleType = "none";
+}*/
